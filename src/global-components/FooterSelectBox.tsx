30e392f48268526444586c310be671799c06c157
Christian Silvestri <christiansilvestri@live.it>
2022-09-07 16:03:04 +0200

    feature/footer fixed width select box

--- src/global-components/FooterSelectBox.tsx
+++ src/global-components/FooterSelectBox.tsx
@@ -7,10 +7,10 @@ function FooterSelectBox() {
 
     return (
         <div className="grid gap-x-4 gap-y-4 md:flex lg:col-span-4 lg:pl-0 xl:col-span-4 xl:col-start-7 2xl:col-start-8 2xl:gap-x-5">
-            <div className="md:w-1/2 border-2 border-gray-400 px-2 h-16 2xl:max-w-[190px] ">
+            <div className="md:w-1/2 border-2 border-gray-400 px-2 h-16 lg:max-w-[190px] lg:min-w-[190px] ">
                 <span className=''>Prezzi Regionali<div>Italia</div></span>
             </div>
-            <div className="md:w-1/2 border-2 border-gray-400 px-2 h-16 2xl:max-w-[190px]">
+            <div className="md:w-1/2 border-2 border-gray-400 px-2 h-16 lg:max-w-[190px] lg:min-w-[190px]">
                 <span>Lingua<div>Italia</div></span>
             </div>
         </div>
