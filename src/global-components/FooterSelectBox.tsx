253e66f4458cc70838b21667dccb860b8e3a95d0
Christian Silvestri <christiansilvestri@live.it>
2022-09-07 15:15:31 +0200

    feature/footer uploading footer without interactivity of select box

--- src/global-components/FooterSelectBox.tsx
+++ src/global-components/FooterSelectBox.tsx
@@ -6,7 +6,7 @@ import React from 'react';
 function FooterSelectBox() {
 
     return (
-        <div className="md:flex lg:col-span-4 grid gap-x-4 gap-y-4 lg:pl-0 xl:col-span-4 xl:col-start-7 2xl:gap-x-5 2xl:pl-20">
+        <div className="grid gap-x-4 gap-y-4 md:flex lg:col-span-4 lg:pl-0 xl:col-span-4 xl:col-start-7 2xl:col-start-8 2xl:gap-x-5">
             <div className="md:w-1/2 border-2 border-gray-400 px-2 h-16 2xl:max-w-[190px] ">
                 <span className=''>Prezzi Regionali<div>Italia</div></span>
             </div>
