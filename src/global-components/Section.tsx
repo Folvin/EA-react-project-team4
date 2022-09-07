d2a717f67f2d6754832170490401ea7bbcaedd3e
Kevin Alessi <minicarota15@gmail.com>
2022-09-07 17:05:14 +0200

    fix(Section-component): now accept optional style prop

--- src/global-components/Section.tsx
+++ src/global-components/Section.tsx
@@ -2,10 +2,11 @@ import React from "react";
 
 interface Props {
   children: React.ReactNode;
+  style?: string;
 }
 
-function Section({children}: Props) {
-  return <section className="px-[8%]">{children}</section>;
+function Section({children, style}: Props) {
+  return <section className={`px-[8%] ${style}`}>{children}</section>;
 }
 
 export default Section;
