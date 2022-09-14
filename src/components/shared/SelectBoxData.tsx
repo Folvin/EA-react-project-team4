54cb24acbb5188b326707bfb993a246f5c8ccf99
Christian Silvestri <101564028+Christian-Silvestri@users.noreply.github.com>
2022-09-14 14:17:14 +0200

    Update SelectBoxData.tsx

--- src/components/shared/SelectBoxData.tsx
+++ src/components/shared/SelectBoxData.tsx
@@ -1,5 +1,5 @@
-import { Fragment, useState } from 'react'
-import { Listbox, Transition } from '@headlessui/react'
+import { Fragment, useState } from 'react';
+import { Listbox, Transition } from '@headlessui/react';
 
 
 
