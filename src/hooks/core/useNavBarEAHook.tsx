2ee3042e4e8402c9d16e2bf885065c9d9212dfa8
Kevin Alessi <minicarota15@gmail.com>
2022-09-21 16:51:35 +0200

    style: formatted and corrected dependency array warning

--- src/hooks/core/useNavBarEAHook.tsx
+++ src/hooks/core/useNavBarEAHook.tsx
@@ -1,77 +1,72 @@
-import { useEffect, useState } from "react";
+import {useCallback, useEffect, useState} from "react";
 
 function useNavBarEAHook(menuRef: any) {
+  /* States that control the NavBarEA menus */
 
-    /* States that control the NavBarEA menus */
+  const [userMenu, setUserMenu] = useState("top-[-400px]");
 
-    const [userMenu, setUserMenu] = useState('top-[-400px]');
+  const [helpMenu, setHelpMenu] = useState("top-[-400px]");
 
-    const [helpMenu, setHelpMenu] = useState('top-[-400px]');
+  /* Function to handle the opening and closing of the NavBarEA menus */
 
-    /* Function to handle the opening and closing of the NavBarEA menus */
+  function handleToggle(event: React.MouseEvent<HTMLImageElement>) {
+    let target = event.target as HTMLImageElement;
 
-    function handleToggle(event: React.MouseEvent<HTMLImageElement>) {
+    if (target.id === "user") {
+      if (helpMenu === "top-10") {
+        setHelpMenu("top-[-400px]");
+      }
 
-        let target = event.target as HTMLImageElement;
+      if (userMenu === "top-[-400px]") {
+        setUserMenu("top-10");
+      } else {
+        setUserMenu("top-[-400px]");
+      }
+    } else if (target.id === "help") {
+      if (userMenu === "top-10") {
+        setUserMenu("top-[-400px]");
+      }
 
-        if (target.id === 'user') {
-
-            if (helpMenu === 'top-10') {
-                setHelpMenu('top-[-400px]');
-            }
-
-            if (userMenu === 'top-[-400px]') {
-                setUserMenu('top-10')
-            } else {
-                setUserMenu('top-[-400px]')
-            }
-        }
-
-        else if (target.id === 'help') {
-
-            if (userMenu === 'top-10') {
-                setUserMenu('top-[-400px]')
-            }
-
-            if (helpMenu === 'top-[-400px]') {
-                setHelpMenu('top-10')
-            } else {
-                setHelpMenu('top-[-400px]')
-            }
-        }
-
-        else {
-            setHelpMenu('top-[-400px]');
-            setUserMenu('top-[-400px]');
-        }
+      if (helpMenu === "top-[-400px]") {
+        setHelpMenu("top-10");
+      } else {
+        setHelpMenu("top-[-400px]");
+      }
+    } else {
+      setHelpMenu("top-[-400px]");
+      setUserMenu("top-[-400px]");
     }
+  }
 
-    /* Side effect and function to handle the closing of the NavBarEA menus when clicking outside of the navbar itself */
+  /* Side effect and function to handle the closing of the NavBarEA menus when clicking outside of the navbar itself */
 
-    useEffect(() => {
-
-        document.addEventListener("mousedown", handleOutsideClicks);
-        return () => {
-            document.removeEventListener("mousedown", handleOutsideClicks);
-        };
-
-    }, [userMenu, helpMenu]);
-
-    const handleOutsideClicks = (event: any) => {
-
-        if ((userMenu === 'top-10' || helpMenu === 'top-10') && menuRef.current && !menuRef.current.contains(event.target as Node)) {
-            setUserMenu('top-[-400px]');
-            setHelpMenu('top-[-400px]');
-        };
+  const handleOutsideClicks = useCallback(
+    (event: any) => {
+      if (
+        (userMenu === "top-10" || helpMenu === "top-10") &&
+        menuRef.current &&
+        !menuRef.current.contains(event.target as Node)
+      ) {
+        setUserMenu("top-[-400px]");
+        setHelpMenu("top-[-400px]");
+      }
+    },
+    [helpMenu, menuRef, userMenu]
+  );
+  useEffect(() => {
+    document.addEventListener("mousedown", handleOutsideClicks);
+    return () => {
+      document.removeEventListener("mousedown", handleOutsideClicks);
     };
+  }, [userMenu, helpMenu, handleOutsideClicks]);
 
-    return ({
-        userMenu,
-        helpMenu,
-        handleToggle,
-        setUserMenu,
-        setHelpMenu
-    });
+  return {
+    userMenu,
+    helpMenu,
+    handleToggle,
+    setUserMenu,
+    setHelpMenu,
+  };
 }
 
 export default useNavBarEAHook;
