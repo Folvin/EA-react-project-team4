b6322e4020074c8318632e923d6c2fee1aaa3478
Kevin Alessi <minicarota15@gmail.com>
2022-09-26 19:03:04 +0200

    style: prove formatted

--- src/Prove.tsx
+++ src/Prove.tsx
@@ -1,24 +1,5 @@
-import NavigationBar from "./components/shared/NavigationBar";
-import ApexConfig from "./components/config/ApexConfig";
-import {Link} from "react-router-dom";
-
 function Prove() {
-  const config = ApexConfig;
-
-  return (
-    <div>
-      <NavigationBar
-        navColor={config.navColor}
-        textColor={config.textColor}
-        hoverColor={config.hoverColor}
-        burgerColor={config.burgerColor}
-        logo={config.logo}
-        buyButtonConfig={config.buyButtonConfig}
-        buyButtonColor={config.buyButtonColor}>
-        <Link to="/bruh">BRUH</Link>
-      </NavigationBar>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default Prove;
