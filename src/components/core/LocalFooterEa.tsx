1f95a98aeb3955de53f50e164450c77b15ed9f0f
Christian Silvestri <christiansilvestri@live.it>
2022-09-27 17:37:42 +0200

    export links into a component

--- src/components/core/LocalFooterEa.tsx
+++ src/components/core/LocalFooterEa.tsx
@@ -2,8 +2,18 @@ import Section from "../shared/Section";
 import TextWrap from "../shared/TextWrap";
 import { Link } from 'react-router-dom';
 
+const links = [
+  { name: 'Lavora con noi', path: '#' },
+  { name: 'Dirigenza', path: '#' },
+  { name: "Rapporto sull'impatto", path: '#' },
+  { name: 'Attività politiche', path: '#' },
+  { name: 'Segnala un problema', path: '#' },
+  { name: 'Dichiarazione globale sul rispetto dei diritti umani', path: '#' },
+  { name: 'Sicurezza e privacy', path: '#' },
+]
 
 function LocalFooterEa() {
+
   return (
     <Section className="bg-gray-200 py-8">
       <div>
@@ -13,19 +23,25 @@ function LocalFooterEa() {
           <p className="pb-4 text-sm">**L'offerta scade il 30 settembre 2022 alle ore 10:00 PT. Solo per i nuovi abbonati. Dopo il primo mese, l'abbonamento si rinnoverà automaticamente ogni mese al prezzo attuale dell'abbonamento mensile fino all'annullamento.</p>
           <hr style={{ borderColor: 'gray' }} />
         </div>
-        <TextWrap >
-          <Link to="#" className="hover:text-red-600">Lavora con noi</Link>
-          <Link to="#" className="hover:text-red-600">Dirigenza</Link>
-          <Link to="#" className="hover:text-red-600">Rapporto sull'impatto</Link>
-          <Link to="#" className="hover:text-red-600">Attività ppolitiche</Link>
-          <Link to="#" className="hover:text-red-600">Segnala un problema</Link>
-          <Link to="#" className="hover:text-red-600">Dichiarazione globale sul rispetto dei diritti umani</Link>
-          <Link to="#" className="hover:text-red-600">Sicurezza e privacy</Link>
-        </TextWrap>
-
+        <LocalFooterLinks arr={links} />
       </div>
-
     </Section>
+  )
+}
+
+
+interface Props {
+  name: string;
+  path?: any;
+}
+
+function LocalFooterLinks({ arr }: { arr: Array<Props> }) {
+  return (
+    <TextWrap>
+      {arr.map((link, index) => {
+        return <Link to={link.path} key={index} className='hover:text-red-600'>{link.name}</Link>
+      })}
+    </TextWrap>
   )
 }
 
