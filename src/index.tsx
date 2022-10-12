dc688d532e06822c9fe27ad8c169ba5b64dcdec2
Kevin Alessi <minicarota15@gmail.com>
2022-10-12 18:47:33 +0200

    fix: scroll to top on routing

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,29 @@
-import React from "react";
+import React, {useLayoutEffect} from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import {BrowserRouter} from "react-router-dom";
+import {BrowserRouter, useLocation} from "react-router-dom";
 import Routing from "./Routing";
 import Footer from "./components/core/Footer";
 import LoginBar from "./components/core/LoginBar";
+
+const Wrapper = ({children}: any) => {
+  const location = useLocation();
+  useLayoutEffect(() => {
+    document.documentElement.scrollTo(0, 0);
+  }, [location.pathname]);
+  return children;
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <LoginBar />
-      <div className="h-[96px]"></div>
-      <Routing />
-      <Footer />
+      <Wrapper>
+        <LoginBar />
+        <div className="h-[96px]"></div>
+        <Routing />
+        <Footer />
+      </Wrapper>
     </BrowserRouter>
   </React.StrictMode>
 );
